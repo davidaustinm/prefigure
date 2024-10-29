e1ff7ef83187395cec4dc36f1609fb8cc2501a24
David Austin <david.austin.m@gmail.com>
2024-10-29 09:56:38 -0400

    multiples of pi on axes

--- prefig/core/grid_axes.py
+++ prefig/core/grid_axes.py
@@ -24,7 +24,9 @@ grid_delta = {2: 0.1, 3: 0.25, 4: 0.25, 5: 0.5,
               11: 0.5, 12: 1, 13: 1, 14: 1, 15: 1, 16: 1, 
               17: 1, 18: 1, 19: 1, 20: 1 }
 
-def find_gridspacing(coordinate_range):
+def find_gridspacing(coordinate_range, pi_format=False):
+    if pi_format:
+        coordinate_range = [c/math.pi for c in coordinate_range]
     dx = 1
     distance = abs(coordinate_range[1]-coordinate_range[0])
     while distance > 10:
@@ -41,6 +43,8 @@ def find_gridspacing(coordinate_range):
     else:
         x0 = dx * math.ceil(coordinate_range[0]/dx-1e-10)
         x1 = dx * math.floor(coordinate_range[1]/dx+1e-10)
+    if pi_format:
+        return (x0*math.pi, dx*math.pi, x1*math.pi)
     return (x0, dx, x1)
 
 # Add a graphical element for a grid.  All the grid lines sit inside a group
@@ -64,18 +68,21 @@ def grid(element, diagram, parent, outline_status):
 
     bbox = diagram.bbox()
     spacings = element.get('spacings', None)
+    h_pi_format = element.get('h-pi-format', 'no') == 'yes'
+    v_pi_format = element.get('v-pi-format', 'no') == 'yes'
+    
     if spacings is not None:
         rx, ry = un.valid_eval(spacings)
     else:
         rx = element.get('hspacing')
         if rx is None:
-            rx = find_gridspacing((bbox[0], bbox[2])) 
+            rx = find_gridspacing((bbox[0], bbox[2]), h_pi_format) 
         else:
             rx = un.valid_eval(rx)
 
         ry = element.get('vspacing')
         if ry is None:
-            ry = find_gridspacing((bbox[1], bbox[3]))
+            ry = find_gridspacing((bbox[1], bbox[3]), v_pi_format)
         else:
             ry = un.valid_eval(ry)
 
@@ -99,7 +106,9 @@ label_delta = {2: 0.2, 3: 0.5, 4: 0.5, 5: 1,
                12: 2, 13: 2, 14: 2, 15: 2, 16: 2, 17: 2,
                18: 2, 19: 2, 20: 2}
 
-def find_label_positions(coordinate_range):
+def find_label_positions(coordinate_range, pi_format = False):
+    if pi_format:
+        coordinate_range = [c/math.pi for c in coordinate_range]
     dx = 1
     distance = abs(coordinate_range[1]-coordinate_range[0])
     while distance > 10:
@@ -121,6 +130,40 @@ def find_label_positions(coordinate_range):
         x0 = dx * math.ceil(coordinate_range[0]/dx-1e-10)
         x1 = dx * math.floor(coordinate_range[1]/dx+1e-10)
     return (x0, dx, x1)
+
+# find a string representation of x*pi
+def get_pi_text(x):
+    if abs(abs(x) - 1) < 1e-10:
+        if x < 0:
+            return '-\pi'
+        return '\pi'
+
+    if abs(x - round(x)) < 1e-10:
+        return str(round(x))+'\pi'
+    if abs(4*x - round(4*x)) < 1e-10:
+        num = round(4*x)
+        if num == -1:
+            return '-\pi/4'
+        if num == 1:
+            return '\pi/4'
+        if num % 2 == 1:
+            return str(num)+'\pi/4'
+    if abs(2*x - round(2*x)) < 1e-10:
+        num = round(2*x)
+        if num == -1:
+            return '-\pi/2'
+        if num == 1:
+            return '\pi/2'
+        return str(num)+'\pi/2'
+    if abs(3*x - round(3*x)) < 1e-10:
+        num = round(3*x)
+        if num == -1:
+            return '-\pi/3'
+        if num == 1:
+            return '\pi/3'
+        return str(num)+'\pi/3'
+    return '{0:g}\pi'.format(x)
+    
 
 # Add a graphical element for axes.  All the axes sit inside a group
 # There are a number of options to add: labels, tick marks, etc
@@ -216,11 +259,17 @@ def axes(element, diagram, parent, outline_status):
     hticks = element.get('hticks', None)
     vticks = element.get('vticks', None)
 
+    h_pi_format = element.get('h-pi-format', 'no') == 'yes'
+    v_pi_format = element.get('v-pi-format', 'no') == 'yes'
+    
     hlabels = element.get('hlabels')
     if hlabels is None:
-        hlabels = find_label_positions((bbox[0], bbox[2]))
+        hlabels = find_label_positions((bbox[0], bbox[2]),
+                                       pi_format = h_pi_format)
     else:
         hlabels = un.valid_eval(hlabels)
+        if h_pi_format:
+            hlabels = 1/math.pi * hlabels
 
     g_hticks = ET.SubElement(axes, 'g',
                              attrib={
@@ -252,21 +301,26 @@ def axes(element, diagram, parent, outline_status):
             g_hticks.append(line_el)
             x += hticks[1]
 
+    h_scale = 1
+    if h_pi_format:
+        h_scale = math.pi
     if decorations == 'yes' or element.get('hlabels', None) is not None:
         x = hlabels[0]
         tick_direction = 1
         if top_labels:
             tick_direction = -1
         while x <= hlabels[2]:
-            if any([abs(x-p) < position_tolerance for p in [bbox[0], bbox[2],0]]):
+            if any([abs(x*h_scale-p) < position_tolerance for p in [bbox[0], bbox[2],0]]):
                 x += hlabels[1]
                 continue
 
             xlabel = ET.Element('label')
             math_element = ET.SubElement(xlabel, 'm')
             math_element.text = r'\text{'+'{0:g}'.format(x)+'}'
+            if h_pi_format:
+                math_element.text = get_pi_text(x)
 
-            xlabel.set('p', '({},{})'.format(x, y_axis_location))
+            xlabel.set('p', '({},{})'.format(x*h_scale, y_axis_location))
             if diagram.output_format() == 'tactile':
                 if top_labels:
                     xlabel.set('alignment', 'hat')
@@ -284,7 +338,7 @@ def axes(element, diagram, parent, outline_status):
             xlabel.set('clear-background', 'no')
             label.label(xlabel, diagram, parent, outline_status)
 
-            p = diagram.transform((x,y_axis_location))
+            p = diagram.transform((x*h_scale,y_axis_location))
             line_el = line.mk_line((p[0], p[1]+tick_direction*ticksize[0]),
                                    (p[0], p[1]-tick_direction*ticksize[1]),
                                    diagram,
@@ -296,9 +350,13 @@ def axes(element, diagram, parent, outline_status):
 
     vlabels = element.get('vlabels')
     if vlabels is None:
-        vlabels = find_label_positions((bbox[1], bbox[3]))
+        vlabels = find_label_positions((bbox[1], bbox[3]),
+                                       pi_format = v_pi_format)
     else:
         vlabels = un.valid_eval(vlabels)
+        if v_pi_format:
+            vlabels = 1/math.pi * vlabels
+            
     g_vticks = ET.SubElement(axes, 'g', attrib={
                               'type': 'vertical ticks'
                               }
@@ -324,21 +382,26 @@ def axes(element, diagram, parent, outline_status):
             g_vticks.append(line_el)
             y += vticks[1]
 
+    v_scale = 1
+    if v_pi_format:
+        v_scale = math.pi
     if decorations == 'yes' or element.get('vlabels', None) is not None:
         y = vlabels[0]
         tick_direction = 1
         if right_labels:
             tick_direction = -1
         while y <= vlabels[2]:
-            if any([abs(y-p) < position_tolerance for p in [bbox[1], bbox[3], 0]]):
+            if any([abs(y*v_scale-p) < position_tolerance for p in [bbox[1], bbox[3], 0]]):
                 y += vlabels[1]
                 continue
 
             ylabel = ET.Element('label')
             math_element = ET.SubElement(ylabel, 'm')
             math_element.text = r'\text{'+'{0:g}'.format(y)+'}'
+            if v_pi_format:
+                math_element.text = get_pi_text(y)
             # process as a math number
-            ylabel.set('p', '({},{})'.format(x_axis_location, y))
+            ylabel.set('p', '({},{})'.format(x_axis_location, y*v_scale))
 
             if diagram.output_format() == 'tactile':
                 if right_labels:
@@ -357,7 +420,7 @@ def axes(element, diagram, parent, outline_status):
 
             ylabel.set('clear-background', 'no')
             label.label(ylabel, diagram, parent, outline_status)
-            p = diagram.transform((x_axis_location, y))
+            p = diagram.transform((x_axis_location, y*v_scale))
             line_el = line.mk_line((p[0]-tick_direction*ticksize[0], p[1]),
                                    (p[0]+tick_direction*ticksize[1], p[1]),
                                    diagram,
@@ -428,6 +491,11 @@ def grid_axes(element, diagram, parent, outline_status):
         el.set('hlabels', element.get('hlabels'))
     if element.get('vlabels') is not None:
         el.set('vlabels', element.get('vlabels'))
+    if element.get('h-pi-format') is not None:
+        el.set('h-pi-format', element.get('h-pi-format'))
+    if element.get('v-pi-format') is not None:
+        el.set('v-pi-format', element.get('v-pi-format'))
+        
     axes(el, diagram, group, outline_status)
 
     annotation = ET.Element('annotation')
